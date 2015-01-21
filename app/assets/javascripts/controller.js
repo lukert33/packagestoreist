377b21887c68d28e4e950bcb598c0fa16be80b30
Brian Borge & Luke Thomas <pair+admiralrico.lukert33@devbootcamp.com>
2015-01-21 15:55:58 -0500

    Migrate controller.

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -1,1 +1,23 @@
+function StoreClerk(inventory, receipt, view) {
+  this.inventory = inventory;
+  this.receipt = receipt;
+  this.view = view;
+}
 
+StoreClerk.prototype.stockShelf = function() {
+  this.view.$inventoryDisplay.html(this.inventory.makeTidy());
+};
+
+StoreClerk.prototype.sellBooze = function() {
+  this.view.$cart.html(this.receipt.tidyWithTotal());
+};
+
+StoreClerk.prototype.prepareCart = function() {
+  this.view.$cart.droppable({
+    drop: function(event, ui){
+      var itemSku = ui.draggable.attr("id").replace("sku","");
+      this.receipt.addToReceipt(this.inventory.shelf[itemSku]);
+      this.sellBooze();
+    }.bind(this)
+  });
+};
--- app/assets/javascripts/model.js
+++ app/assets/javascripts/model.js
@@ -61,29 +61,7 @@ CustomerReceipt.prototype.tidyWithTotal = function() {
 
 // controller
 // should send the shelf abd CustomerReceipt to the view for rendering
-function StoreClerk(inventory, receipt, view) {
-  this.inventory = inventory;
-  this.receipt = receipt;
-  this.view = view;
-};
 
-StoreClerk.prototype.stockShelf = function() {
-  this.view.$inventoryDisplay.html(this.inventory.makeTidy());
-};
-
-StoreClerk.prototype.sellBooze = function() {
-  this.view.$cart.html(this.receipt.tidyWithTotal());
-};
-
-StoreClerk.prototype.prepareCart = function() {
-  this.view.$cart.droppable({
-    drop: function(event, ui){
-      var itemSku = ui.draggable.attr("id").replace("sku","");
-      this.receipt.addToReceipt(this.inventory.shelf[itemSku]);
-      this.sellBooze();
-    }.bind(this)
-  });
-};
 
 // view
 // listening for events and sending them to the controller
